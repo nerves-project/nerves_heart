91c989784912ffc8c2f088a49638fb494dda1a83
Jon Thacker <jon@sparkmeter.io>
2020-08-28 19:11:00 -0400

    Fix typo "of" -> "off"

--- src/heart.c
+++ src/heart.c
@@ -321,7 +321,7 @@ static int message_loop()
                 case SHUT_DOWN:
                     return R_SHUT_DOWN;
                 case SET_CMD:
-                    /* If the user specifies "attack", turn of the hw watchdog petter to verify that the system reboots. */
+                    /* If the user specifies "attack", turn off the hw watchdog petter to verify that the system reboots. */
                     if (mp->len > 6 && memcmp(mp->fill, "attack", 6) == 0) {
                         print_log("heart: Petting of the hardware watchdog is disabled. System should reboot momentarily.");
 
